d0334f73837073a5f4eeb88d5b5d21b7412546e4
sandeep231098 <sandeep2310981@gmail.com>
2026-07-22 23:22:20 +0530

    build: convert backend to multi-module maven architecture

--- backend/auth-service/src/main/java/com/apexbank/auth/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/apexbank/auth/config/SecurityConfig.java
@@ -5,11 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
