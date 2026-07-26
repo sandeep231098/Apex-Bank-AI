bf8195f90c4960428e16f9b2962a87da00fc4f90
sandeep231098 <sandeep2310981@gmail.com>
2026-07-26 19:52:38 +0530

    feat: transaction module with search, statement, swagger and exception handling

--- backend/transaction-service/src/main/java/com/apexbank/transaction/repository/TransactionRepository.java
+++ backend/transaction-service/src/main/java/com/apexbank/transaction/repository/TransactionRepository.java
@@ -1,11 +1,14 @@
 package com.apexbank.transaction.repository;
 
+import com.apexbank.common.enums.TransactionType;
 import com.apexbank.transaction.entity.Transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,4 +24,16 @@ public interface TransactionRepository extends
             UUID accountId,
             Pageable pageable
     );
+    @Query("""
+       SELECT COALESCE(SUM(t.amount), 0)
+       FROM Transaction t
+       WHERE t.transactionType = :transactionType
+       """)
+    BigDecimal getTotalAmountByTransactionType(TransactionType transactionType);
+
+    @Query("""
+       SELECT COUNT(t)
+       FROM Transaction t
+       """)
+    Long getTotalTransactions();
 }
